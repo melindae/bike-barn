adb72417bd1291016724b0f0700152bb1299b7a8
melindae <melindae@gmail.com>
2015-06-05 14:07:17 +0000

    added edit notes ability

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,18 +20,13 @@ $locationProvider.html5Mode(true);
   })
   .state('quickInv', {
     url: '/quick-inventory',
-    controller:'detailCtrl',
+    controller:'listCtrl',
     templateUrl: 'templates/quick-inventory.html',
   })
   .state('detailInv', {
     url: '/detailed-inventory',
-    controller:'detailCtrl',
+    controller:'listCtrl',
     templateUrl: 'templates/detailed-inventory.html',
-  })
-  .state('editBike', {
-    url: '/edit-bike',
-    controller:'editCtrl',
-    templateUrl: 'templates/edit-bike.html',
   })
   .state('otherwise', {
     url: '*path',
@@ -66,93 +61,13 @@ bikeBarn.controller('addCtrl', function($scope, $firebaseArray) {
   };
 })
 
-bikeBarn.controller('detailCtrl', function($scope, $firebaseArray) {
+bikeBarn.controller('listCtrl', function($scope, $firebaseArray) {
   var bikeArray = new Firebase("https://bike-barn.firebaseio.com/bikes");
 
   $scope.bikes = $firebaseArray(bikeArray);
 
-  $scope.edit = function(bike) {
-    var bikeIndex = $scope.bikes.indexOf(bike);
-      console.log(bikeIndex);
-  };
+  $scope.editNotes = function(bike) {
+    $scope.bikeIndex = $scope.bikes.indexOf(bike);
+    $scope.newBikeNote = angular.copy(bike);
+  }
 })
-
-bikeBarn.controller('editCtrl', function($scope, $firebaseArray) {
-  var bikeArray = new Firebase("https://bike-barn.firebaseio.com/bikes");
-
-  $scope.bikes = $firebaseArray(bikeArray);
-
-  $scope.savebike = function() {
-    $scope.bikes.$add({
-      'ready': $scope.bk.ready,
-      'ident':$scope.bk.ident,
-      'year': $scope.bk.year,
-      'make': $scope.bk.make,
-      'model': $scope.bk.model,
-      'maincolor': $scope.bk.maincolor,
-      'maintype': $scope.bk.maintype,
-      'sn': $scope.bk.sn,
-      'notes': $scope.bk.notes,
-      'empty1': 0,
-      'empty2': 0,
-      'empty3': 0,
-      'empty4': 0,
-    });
-  };
-})
-
-
-
-
-
-// bikeBarn.directive('editBike', function() {
-//   return {
-//     restrict: 'A',
-//     template: "<td> {{bike.notes}}<button class='edit-button' ng-click='edit(bike)'> <a ui-sref='editBike'>edit</a> </button> </td>",
-//     link: function($scope, element, attrs) {
-//       $scope.edit = function(bike) {
-//         $scope.model = 'murgle'        
-//         var bikeIndex = $scope.bikes.indexOf(bike);
-//         //$scope.model = $scope.bikes[bikeIndex].model;
-//         var qqq = $scope.bikes[bikeIndex].notes
-//         $scope.notes = qqq;
-//         console.log();
-//         console.log(qqq);
-//       }
-//     }
-//   }
-// })
-
-//   change state
-// doThings.directive('taskComplete', function() {
-//    return {
-//     restrict: 'A',
-//     template: "<button class='tableDone fa fa-check-square-o' ng-click='taskDone(task)'></button>",
-
-//     link: function($scope, element, attrs) {
-//       $scope.taskDone = function(task) {
-//         var taskIndex = $scope.tasks.indexOf(task);
-//         $scope.tasks[taskIndex].done = "Yes!";
-//         $scope.tasks.$save(taskIndex).then(function(fireRef) {
-//           fireRef.key() === $scope.tasks[taskIndex].$id;
-//         });
-//       }
-//     }
-//   }
-// });
-
-// // delete entry
-// doThings.directive('taskDelete', function() {
-//    return {
-//     restrict: 'A',
-//     template: "<button class='tableDone fa fa-minus-square-o' ng-click='taskDelete(task)'></button>",
-//     link: function($scope, element, attrs) {
-//       $scope.taskDelete = function(task) {
-//         var taskIndex = $scope.tasks.indexOf(task);
-//         $scope.tasks.$remove(taskIndex).then(function(fireRef) {
-//           fireRef.key() === $scope.tasks[taskIndex].$id;
-//         });
-//       }
-//     }
-//   }
-// });
